1ecc4ee04464b840487427941922ae9e3a29bbcf
Oom Lout <aaron@oomlout.com>
2023-08-05 21:18:11 +0100

    added image scaling

--- oom_base.py
+++ oom_base.py
@@ -1,5 +1,63 @@
+import os
 
+#image manipulation
+def generate_image(**kwargs):
+    filename = kwargs['filename']
+    resolution = kwargs['resolution']
+    overwrite = kwargs.get('overwrite', False)
+    file_out = filename.split(".")[0] + "_" + str(resolution) + "." + filename.split(".")[1]
+    #skip if overwrite is false and the file already exists
+    if not overwrite:
+        if os.path.isfile(file_out):
+            return
+    
+    #if the image is already a resized on skip it chjeck for all numbers 100 or more
+    if "_" in filename:
+        if filename.split("_")[-1].split(".")[0].isdigit():
+            if int(filename.split("_")[-1].split(".")[0]) >= 100:
+                return
+    
+    #open the image file png or jpg
+    from PIL import Image
+    try:
+        im = Image.open(filename)
+        #scale the image so the largest side is reolution pixels wide
+        width, height = im.size
+        if width > height:
+            scale = resolution / width
+        else:
+            scale = resolution / height
+        new_width = int(width * scale)
+        new_height = int(height * scale)
+        im = im.resize((new_width, new_height), Image.ANTIALIAS)
+        #save the image
+        im.save(file_out)
+    except:
+        print("Error with image: " + filename)
+        pass
 
+# string manipulation
+
+def remove_special_characters(string):
+    symbol_name = string
+    symbol_name = symbol_name.replace('/', '_')
+    symbol_name = symbol_name.replace('\\', '_')
+    symbol_name = symbol_name.replace(':', '_')
+    symbol_name = symbol_name.replace('*', '_')
+    symbol_name = symbol_name.replace('?', '_')
+    symbol_name = symbol_name.replace('"', '_')
+    symbol_name = symbol_name.replace('<', '_')
+    symbol_name = symbol_name.replace('>', '_')
+    symbol_name = symbol_name.replace('|', '_')
+    symbol_name = symbol_name.replace('-', '_')        
+    symbol_name = symbol_name.replace('+', '_')
+    symbol_name = symbol_name.replace(' ', '_')
+    symbol_name = symbol_name.replace('.', '_')
+    symbol_name = symbol_name.replace('__', '_')
+    symbol_name = symbol_name.replace('__', '_')
+    symbol_name = symbol_name.replace('__', '_')
+    symbol_name = symbol_name.replace('__', '_')
+    return symbol_name
 
 
 
--- oom_kiutils.py
+++ oom_kiutils.py
@@ -1,22 +1,46 @@
-
+import oom_base
 
 def symbol_change_name_oomp(**kwargs):
     library_name = kwargs['library_name']    
     repo = kwargs['repo']
-    owner = repo['owner']
-    entry_name = kwargs['entry_name']
+    owner = repo['owner']    
     symbol = kwargs['symbol']
+    try:
+        entry_name = oom_base.remove_special_characters(symbol.entryName)
+        entry_name_original = symbol.entryName
+    except:
+        entry_name = oom_base.remove_special_characters(symbol.id)
+        entry_name_original = symbol.id
 
-
-    entry_name = f'{owner}_{library_name}_{symbol.entryName}'                    
-    entry_name_original = symbol.entryName
-    symbol.entryName = symbol.entryName.replace(entry_name_original, entry_name)
+                   
+    entry_name = oom_base.remove_special_characters(f'{owner}_{library_name}_{entry_name}')
+    entry_name = entry_name.lower()                    
+    
+    try:
+        extra_entry_name = symbol.entryName.replace(entry_name_original, entry_name)
+        extra_entry_name = oom_base.remove_special_characters(extra_entry_name)
+        symbol.entryName = symbol.entryName.replace(entry_name_original, extra_entry_name)
+    except:
+        extra_entry_name = symbol.entryName.replace(entry_name_original, entry_name)
+        extra_entry_name = oom_base.remove_special_characters(extra_entry_name)
+        symbol.id = symbol.id.replace(entry_name_original, extra_entry_name)
     for unit in symbol.units:
-        unit.entryName = unit.entryName.replace(entry_name_original, entry_name)
+        try:
+            test = unit.entryName
+            extra_entry_name = unit.entryName.replace(entry_name_original, entry_name)
+            extra_entry_name = oom_base.remove_special_characters(extra_entry_name)
+            unit.entryName = extra_entry_name
+        except:
+            extra_entry_name = unit.id.replace(entry_name_original, entry_name)
+            extra_entry_name = oom_base.remove_special_characters(extra_entry_name)
+            unit.id = unit.id.replace(entry_name_original, entry_name)
     #if extends
+    if "10m03" in entry_name:
+        pass
     if symbol.extends != None:
         extend_extra = f'{owner}_{library_name}'
-        symbol.extends = f'{extend_extra}_{symbol.extends}'
+        extend_extra = oom_base.remove_special_characters(f'{extend_extra}_{symbol.extends}')
+        symbol.extends = extend_extra
 
     return symbol
     
