8153c650ffb385eed5c2cadd8a2c8515afe833b3
Oom Lout <aaron@oomlout.com>
2023-10-08 16:15:43 +0100

    readme work

--- action_generate_readme_outputs.py
+++ action_generate_readme_outputs.py
@@ -8,6 +8,7 @@ def main_single_directory():
     directory = os.getcwd()
     #directory = "C:\GH\oomlout_ibbc_breakout_board_holder_oobb_3_2"
     #directory = "C:\GH\oomlout_oomp_electronic_project_usb_switch"
+    #directory = r"C:\GH\oomlout_oomp_electronic_device_aliexpress_amp_meter_10_amp_0_28_inch_display"
     oom_markdown.generate_readme_project(directory=directory)
     
 
