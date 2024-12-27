f356e522574c7b28bef25e618700d6dc2b5a7df5
Oom Lout <aaron@oomlout.com>
2024-12-27 09:47:34 +0000

    adde dopening output directories

--- a_ai_1111.bat
+++ a_ai_1111.bat
@@ -1,3 +1,5 @@
 start https://docs.google.com/document/d/1cVfdixwzDNcDbn0nQWSJJvEgWKCzFb_DfIZe4DCi9Jc/edit?tab=t.0
+start C:\sd1\webui\outputs\extras-images\
+start C:\sd1\webui\outputs\txt2img-images
 cd c:\sd1
 run.bat
