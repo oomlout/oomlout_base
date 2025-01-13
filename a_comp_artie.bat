55960c741665cbbcd32315ea1a7193a57361d155
Oom Lout <aaron@oomlout.com>
2025-01-13 09:30:36 +0000

    Base

---
+++ a_comp_artie.bat
@@ -0,0 +1,5 @@
+call code c:\gh\oomlout_oobb_version_4
+call code c:\gh\oomlout_oobb_version_4_generated_parts
+z:
+git clone https://github.com/oomlout/oomlout_oomp_version_1_messy
+call code z:\oomlout_oomp_version_1_messy
