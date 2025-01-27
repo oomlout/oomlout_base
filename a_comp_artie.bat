c726c21a0fad26eeb3e449bbe35658bacde2bcec
Oom Lout <aaron@oomlout.com>
2025-01-27 10:41:04 +0000

    Base

--- a_comp_artie.bat
+++ a_comp_artie.bat
@@ -1,5 +1,7 @@
+call C:\Users\aaron\AppData\Local\GitHubDesktop\GitHubDesktop.exe
 call code c:\gh\oomlout_oobb_version_4
 call code c:\gh\oomlout_oobb_version_4_generated_parts
 z:
 git clone https://github.com/oomlout/oomlout_oomp_version_1_messy
 call code z:\oomlout_oomp_version_1_messy
+
