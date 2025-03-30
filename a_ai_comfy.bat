38df5f38992c5a21a72a5704efd04cf713a99d75
Oom Lout <aaron@oomlout.com>
2025-03-30 21:10:01 +0100

    Base

--- a_ai_comfy.bat
+++ a_ai_comfy.bat
@@ -1,5 +1,15 @@
 start https://docs.google.com/document/d/1cVfdixwzDNcDbn0nQWSJJvEgWKCzFb_DfIZe4DCi9Jc/edit?tab=t.0
-start Z:\sd3\ComfyUI\output
-z:
-cd sd3
-run_nvidia_gpu.bat
+start C:\pinokio\api\comfy.git\app\output
+
+
+
+
+
+
+@REM old
+
+@REM start https://docs.google.com/document/d/1cVfdixwzDNcDbn0nQWSJJvEgWKCzFb_DfIZe4DCi9Jc/edit?tab=t.0
+@REM start Z:\sd3\ComfyUI\output
+@REM z:
+@REM cd sd3
+@REM run_nvidia_gpu.bat
