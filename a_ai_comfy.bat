93ea03d2886e8dba152a79cc8d8fa73991d2eda8
Oom Lout <aaron@oomlout.com>
2025-03-30 20:19:26 +0100

    Base

--- a_ai_comfy.bat
+++ a_ai_comfy.bat
@@ -1,4 +1,5 @@
 start https://docs.google.com/document/d/1cVfdixwzDNcDbn0nQWSJJvEgWKCzFb_DfIZe4DCi9Jc/edit?tab=t.0
-start C:\sd3\ComfyUI\output
-cd c:\sd3
+start Z:\sd3\ComfyUI\output
+z:
+cd sd3
 run_nvidia_gpu.bat
