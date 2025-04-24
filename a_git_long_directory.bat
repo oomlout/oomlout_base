e8fd7baa4879b8a22a10616fc507d1ec36760998
Oom Lout <aaron@oomlout.com>
2025-04-24 15:53:08 +0100

    Update a_git_long_directory.bat

--- a_git_long_directory.bat
+++ a_git_long_directory.bat
@@ -1,6 +1,6 @@
 set "appdata_dir=%LOCALAPPDATA%"
 echo Local App Data Directory is %appdata_dir%
-set "gh_dir=%appdata_dir%\GitHubDesktop\app-3.3.11\resources\app\git\cmd"
+set "gh_dir=%appdata_dir%\GitHubDesktop\app-3.4.19\resources\app\git\cmd"
 cd %gh_dir%
 git config --system core.longpaths true
 pause
