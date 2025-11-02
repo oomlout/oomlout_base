d6dd55c75339e601787111e9e53174f3dd465a9c
Oom Lout <aaron@oomlout.com>
2025-11-02 15:10:07 +0000

    Base

--- a_comp_artie.bat
+++ a_comp_artie.bat
@@ -4,6 +4,7 @@ rem call code c:\gh\oomlout_oobb_version_4_generated_parts
 rem call code C:\gh\oomlout_oomp_part_generation_version_1
 z:
 git clone https://github.com/oomlout/oomlout_oomp_version_1_messy
-hit clone https://github.com/oomlout/oomlout_oomp_version_1_only_yaml
+git clone https://github.com/oomlout/oomlout_oomp_version_1_only_yaml
+start python C:\gh\oomlout_base_webserver_flask_template_oomp\working_web_oomp.py 
 rem call code z:\oomlout_oomp_version_1_messy
 
