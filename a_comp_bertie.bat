6b514819ea8efdf385be247c6ed401f023b1cc76
Oom Lout <aaron@oomlout.com>
2025-12-27 13:52:44 +0000

    Base

--- a_comp_bertie.bat
+++ a_comp_bertie.bat
@@ -14,3 +14,6 @@ start python C:\od\OneDrive\docs\sticker\working_web_oomp.py
 
 start python C:\od\OneDrive\docs\helen_school_english_spelling_editing\working_web.py
 start python C:\od\OneDrive\docs\helen_school_english_spelling_editing\working_web_oomp.py
+
+start python c:\od\OneDrive\docs\ai_generation_from_yaml_launcher\working_web.py
+start python c:\od\OneDrive\docs\ai_generation_from_yaml_launcher\working_web_oomp.py
