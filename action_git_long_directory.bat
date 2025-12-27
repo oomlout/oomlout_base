3d4c2e9bd4d3e87f00aa910c0a50338e8a465a23
Oom Lout <aaron@oomlout.com>
2025-12-27 19:46:44 +0000

    update

--- action_git_long_directory.bat
+++ action_git_long_directory.bat
@@ -1,2 +1,2 @@
-cd C:\Users\aaron\AppData\Local\GitHubDesktop\app-3.5.3\resources\app\git\cmd
+cd C:\Users\aaron\AppData\Local\GitHubDesktop\app-3.5.4\resources\app\git\cmd
 git config --system core.longpaths true
