ae10aae25ec699066b5e5b7f20f59efeb3d2236f
Oom Lout <aaron@oomlout.com>
2025-12-29 23:00:37 +0000

    Base

--- lock/working_all.bat
+++ lock/working_all.bat
@@ -1,1 +1,3 @@
-cd /d "C:\od\OneDrive\docs\ai_generation_from_yaml_launcher" && python working_all.py
+cd "C:\od\OneDrive\docs\helen_school_english_spelling_editing"
+python "working_all.py"
+cd /d "c:\od\OneDrive\docs\ai_generation_from_yaml_launcher" && python working_all.py
