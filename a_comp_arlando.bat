64f7eb3dc06f34abcf63f23099ac2acc4bd16a16
Oom Lout <aaron@oomlout.com>
2026-05-10 18:27:00 +0100

    changes

--- a_comp_arlando.bat
+++ a_comp_arlando.bat
@@ -1,4 +1,5 @@
 start python C:\gh\oomlout_base_webserver_flask_template_oomp\working_web_oomp.py 
+taskkill /IM ollama.exe /F
 start ollama serve
 start open-webui serve
-
+start powershell -NoExit -Command "cd 'C:\od\OneDrive\docs\ai_agent_claude_code_local'; ollama launch claude --model gpt-oss:20b -- --dangerously-skip-permissions"
