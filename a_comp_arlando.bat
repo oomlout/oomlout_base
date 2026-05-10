2653cef139f8505643f232756e8a03c221810d73
Oom Lout <aaron@oomlout.com>
2026-05-10 20:19:19 +0100

    working

--- a_comp_arlando.bat
+++ a_comp_arlando.bat
@@ -1,5 +1,9 @@
 start python C:\gh\oomlout_base_webserver_flask_template_oomp\working_web_oomp.py 
-taskkill /IM ollama.exe /F
-start ollama serve
+
+REM ollama section
+
+start a_comp_arlando_ollama.bat
+
 start open-webui serve
-start powershell -NoExit -Command "cd 'C:\od\OneDrive\docs\ai_agent_claude_code_local'; ollama launch claude --model gpt-oss:20b -- --dangerously-skip-permissions"
+start powershell -NoExit -Command "cd 'C:\od\OneDrive\docs\ai_agent_claude_code_local\test_app_1\glm-4.7-flashq4_K_M'; ollama launch claude --model glm-4.7-flash:q4_K_M -- --dangerously-skip-permissions"
+start "C:\Program Files (x86)\openhardwaremonitor\OpenHardwareMonitor.exe"
